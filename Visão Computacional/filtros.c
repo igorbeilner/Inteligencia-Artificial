36859b14c00d893120ff2c50a9e5efb3f49c1f4a
igorbeilner <igor.beilner@hotmail.com>
2015-10-03 15:58:19 -0300

    Escrita dos resultados das operações em arquivos.

--- Visão Computacional/filtros.c
+++ Visão Computacional/filtros.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
 
 #define RESOLUCAO 13
 
-void media(unsigned char src[RESOLUCAO][RESOLUCAO], int kernel) {
-	unsigned char image[RESOLUCAO][RESOLUCAO];
+void media(unsigned char src[RESOLUCAO][RESOLUCAO][21], unsigned char image[RESOLUCAO][RESOLUCAO][21], int m, int kernel) {
 	int i, j, k, pixel;
 
 	for(i=0; i<RESOLUCAO; i++) {
@@ -14,39 +14,32 @@ void media(unsigned char src[RESOLUCAO][RESOLUCAO], int kernel) {
 			for(k=1; k<=(kernel/2); k++) {
 
 				if((i-k) >= 0) {
-					pixel += src[i-k][j];
+					pixel += src[i-k][j][m];
 					if((j+k) < RESOLUCAO)
-						pixel += src[i-k][j+k];
+						pixel += src[i-k][j+k][m];
 					if((j-k) >= 0)
-						pixel += src[i-k][j-k];
+						pixel += src[i-k][j-k][m];
 				}
 
 				if((i+k) < RESOLUCAO) {
-					pixel += src[i+k][j];
+					pixel += src[i+k][j][m];
 					if((j+k) < RESOLUCAO)
-						pixel += src[i+k][j+k];
+						pixel += src[i+k][j+k][m];
 					if((j-k) >= 0)
-						pixel += src[i+k][j-k];
+						pixel += src[i+k][j-k][m];
 				}
 
 				if((j+k) < RESOLUCAO)
-					pixel += src[i][j+k];
+					pixel += src[i][j+k][m];
 				if((j-k) >= 0)
-					pixel += src[i][j-k];
+					pixel += src[i][j-k][m];
 
 			}
-			pixel += src[i][j];
+			pixel += src[i][j][m];
 
-			image[i][j] = pixel/(kernel*kernel);
+			image[i][j][m] = pixel/(kernel*kernel);
 		}
 	}
-
-	for(i=0; i<RESOLUCAO; i++) {
-		for(j=0; j<RESOLUCAO; j++)
-			printf("%-4d ", image[i][j]);
-		printf("\n");
-	}
-
 }
 
 unsigned char modav(unsigned char *vet, int len) {
@@ -66,8 +59,7 @@ unsigned char modav(unsigned char *vet, int len) {
 	return j;
 }
 
-void moda(unsigned char src[RESOLUCAO][RESOLUCAO], int kernel) {
-	unsigned char image[RESOLUCAO][RESOLUCAO];
+void moda(unsigned char src[RESOLUCAO][RESOLUCAO][21], unsigned char image[RESOLUCAO][RESOLUCAO][21], int m, int kernel) {
 	int i, j, k, l;
 	unsigned char *pixel = (unsigned char*)malloc((kernel*kernel)*(sizeof(unsigned char)));
 
@@ -77,56 +69,50 @@ void moda(unsigned char src[RESOLUCAO][RESOLUCAO], int kernel) {
 			for(k=1; k<=(kernel/2); k++) {
 
 				if((i-k) >= 0) {
-					pixel[l] = src[i-k][j];
+					pixel[l] = src[i-k][j][m];
 					l++;
 					if((j+k) < RESOLUCAO) {
-						pixel[l] = src[i-k][j+k];
+						pixel[l] = src[i-k][j+k][m];
 						l++;
 					}
 
 					if((j-k) >= 0) {
-						pixel[l] = src[i-k][j-k];
+						pixel[l] = src[i-k][j-k][m];
 						l++;
 					}
 				}
 
 				if((i+k) < RESOLUCAO) {
-					pixel[l] = src[i+k][j];
+					pixel[l] = src[i+k][j][m];
 					l++;
 					if((j+k) < RESOLUCAO) {
-						pixel[l] = src[i+k][j+k];
+						pixel[l] = src[i+k][j+k][m];
 						l++;
 					}
 
 					if((j-k) >= 0) {
-						pixel[l] = src[i+k][j-k];
+						pixel[l] = src[i+k][j-k][m];
 						l++;
 					}
 				}
 
 				if((j+k) < RESOLUCAO) {
-					pixel[l] = src[i][j+k];
+					pixel[l] = src[i][j+k][m];
 					l++;
 				}
 				if((j-k) >= 0) {
-					pixel[l] = src[i][j-k];
+					pixel[l] = src[i][j-k][m];
 					l++;
 				}
 
 			}
-			pixel[l] = src[i][j];
+			pixel[l] = src[i][j][m];
 
-			image[i][j] = modav(pixel, kernel);
+			image[i][j][m] = modav(pixel, kernel);
 
 		}
 	}
-	printf("\n");
-
-	for(i=0; i<RESOLUCAO; i++) {
-		for(j=0; j<RESOLUCAO; j++)
-			printf("%-4d ", image[i][j]);
-		printf("\n");
-	}
+	free(pixel);
 }
 
 void merge(unsigned char *vet, int p, int q, int kernel) {
@@ -167,10 +153,10 @@ void merge(unsigned char *vet, int p, int q, int kernel) {
 	for(i=p; i<=q ; i++) {
 		vet[i] = aux[i-p];
 	}
+	free(aux);
 }
 
-void mediana(unsigned char src[RESOLUCAO][RESOLUCAO], int kernel) {
-	unsigned char image[RESOLUCAO][RESOLUCAO];
+void mediana(unsigned char src[RESOLUCAO][RESOLUCAO][21], unsigned char image[RESOLUCAO][RESOLUCAO][21], int m, int kernel) {
 	int i, j, k, l;
 	unsigned char *pixel = (unsigned char*)malloc((kernel*kernel)*(sizeof(unsigned char)));
 
@@ -180,61 +166,113 @@ void mediana(unsigned char src[RESOLUCAO][RESOLUCAO], int kernel) {
 			for(k=1; k<=(kernel/2); k++) {
 
 				if((i-k) >= 0) {
-					pixel[l] = src[i-k][j];
+					pixel[l] = src[i-k][j][m];
 					l++;
 					if((j+k) < RESOLUCAO) {
-						pixel[l] = src[i-k][j+k];
+						pixel[l] = src[i-k][j+k][m];
 						l++;
 					}
 
 					if((j-k) >= 0) {
-						pixel[l] = src[i-k][j-k];
+						pixel[l] = src[i-k][j-k][m];
 						l++;
 					}
 				}
 
 				if((i+k) < RESOLUCAO) {
-					pixel[l] = src[i+k][j];
+					pixel[l] = src[i+k][j][m];
 					l++;
 					if((j+k) < RESOLUCAO) {
-						pixel[l] = src[i+k][j+k];
+						pixel[l] = src[i+k][j+k][m];
 						l++;
 					}
 
 					if((j-k) >= 0) {
-						pixel[l] = src[i+k][j-k];
+						pixel[l] = src[i+k][j-k][m];
 						l++;
 					}
 				}
 
 				if((j+k) < RESOLUCAO) {
-					pixel[l] = src[i][j+k];
+					pixel[l] = src[i][j+k][m];
 					l++;
 				}
 				if((j-k) >= 0) {
-					pixel[l] = src[i][j-k];
+					pixel[l] = src[i][j-k][m];
 					l++;
 				}
 
 			}
-			pixel[l] = src[i][j];
+			pixel[l] = src[i][j][m];
 
 			merge(pixel, 0, l, kernel);
-			image[i][j] = pixel[(l/2)];
+			image[i][j][m] = pixel[(l/2)];
 
 		}
 	}
-	printf("\n");
+	free(pixel);
+}
+
+void roberts(unsigned char src[RESOLUCAO][RESOLUCAO][21], unsigned char image[RESOLUCAO][RESOLUCAO][21], int m) {
+	int i, j;
 
 	for(i=0; i<RESOLUCAO; i++) {
 		for(j=0; j<RESOLUCAO; j++)
-			printf("%-4d ", image[i][j]);
-		printf("\n");
+			if((i+1) < RESOLUCAO && (j+1) < RESOLUCAO)
+				image[i][j][m] = sqrt(pow(src[i][j][m] - src[i+1][j+1][m], 2)) + sqrt(pow(src[i+1][j][m] - src[i][j+1][m], 2));
 	}
-
 }
 
-void imread(char *csvFile, unsigned char image[RESOLUCAO][RESOLUCAO]) {
+void laplaciano(unsigned char src[RESOLUCAO][RESOLUCAO][21], unsigned char image[RESOLUCAO][RESOLUCAO][21], int m) {
+	int i, j, pixel, aux, aux1;
+
+	for(i=0; i<RESOLUCAO; i++) {
+		for(j=0; j<RESOLUCAO; j++) {
+			pixel = 0;
+			aux1 = 0;
+
+			if((i-1) >= 0)
+				pixel += src[i-1][j][m];
+			else aux1++;
+
+			if((j-1) >= 0)
+				pixel += src[i][j-1][m];
+			else aux1++;
+
+			if((j+1) < RESOLUCAO)
+				pixel += src[i][j+1][m];
+			else aux1++;
+
+			if((i+1) < RESOLUCAO)
+				pixel += src[i+1][j][m];
+			else aux1++;
+
+			aux = (4-aux1)*(int)(src[i][j][m]) - pixel;
+
+			if(aux < 0) aux = 0;
+			else if(aux > 255) aux = 255;
+			image[i][j][m] = aux;
+
+		}
+	}
+}
+
+void showImage(unsigned char image[RESOLUCAO][RESOLUCAO][21]) {
+	int i, j, k;
+
+	for(k=0; k<20; k++) {
+
+			printf("\nImagem %d:\n", k+1);
+			for(i=0; i<RESOLUCAO; i++) {
+				for(j=0; j<RESOLUCAO; j++)
+					printf("%-3d ", image[i][j][k]);
+				printf("\n");
+			}
+
+	}
+}
+
+void imRead(char *csvFile, unsigned char image[RESOLUCAO][RESOLUCAO][21], int m) {
 	char 	caracter,
 			valor[sizeof(int)];
 
@@ -249,46 +287,131 @@ void imread(char *csvFile, unsigned char image[RESOLUCAO][RESOLUCAO]) {
 		fseek(F, -1, 1);
 
 		if(fread(&caracter, sizeof(char), 1, F)){
-			if(caracter == ',') {
-				image[k][l] = 0;
-				for(j=0; j<i; j++) image[k][l] += (valor[j]-0x30)*(pow(10, (i-1)-j));
+			if(caracter == ',') {			// Lê até o separador da coluna
+				image[k][l][m] = 0;
+				for(j=0; j<i; j++) image[k][l][m] += (valor[j]-0x30)*(pow(10, (i-1)-j));	// Converte de ASCII para decimal
 				l++;
 				i=0;
 				continue;
 			}
-			if(caracter == '\n') {
-				image[k][l] = 0;
-				for(j=0; j<i-1; j++) image[k][l] += (valor[j]-0x30)*(pow(10, (i-2)-j));
+			if(caracter == '\n') {			// Lê até o fim da linha
+				image[k][l][m] = 0;
+				for(j=0; j<i-1; j++) image[k][l][m] += (valor[j]-0x30)*(pow(10, (i-2)-j));
 				k++; l=0;
 				i=0;
 				continue;
 			}
-			valor[i] = caracter;
+			valor[i] = caracter;	// Atribui o caracter à respectiva cada decimal
 			i++;
 		}
 	}
-
-	for(i=0; i<RESOLUCAO; i++) {
-		for(j=0; j<RESOLUCAO; j++)
-			printf("%-4d ", image[i][j]);
-		printf("\n");
-	}
-
+	fclose(F);
 }
 
-int main(int argc, char *argv[]) {
-	unsigned char image[RESOLUCAO][RESOLUCAO];
+void imWrite(char *csvFile, unsigned char image[RESOLUCAO][RESOLUCAO][21], int m) {
+	char 	valor[4], aux[2];
 
-	imread(argv[1], image);
+	int 	i, j;
 
-	printf("\nMedia:\n");
+	FILE 	*F = fopen(csvFile, "w+");
 
-	media(image, 3);
-	printf("\nMediana:");
-	mediana(image, 3);
-	printf("\nModa:");
-	moda(image, 3);
-	printf("\n");
+	aux[0] = ',';
+	aux[1] = '\n';
+
+	if(F == NULL)
+		printf("Arquivo nao encontrado\n");
+
+
+	for(i=0; i<RESOLUCAO; i++) {
+		for(j=0; j<RESOLUCAO; j++) {
+			valor[0] = image[i][j][m]/100 + 0x30;
+			valor[1] = (image[i][j][m]%100)/10 +0x30;
+			valor[2] = image[i][j][m]%10 + 0x30;
+			valor[3] = '\0';
+
+			if(valor[0] != 0x30)
+				fwrite(&valor[0], sizeof(char), 1, F);
+
+			if(!(valor[0] == 0x30 && valor[1] == 0x30))
+				fwrite(&valor[1], sizeof(char), 1, F);
+
+			fwrite(&valor[2], sizeof(char), 1, F);
+
+			if(j < RESOLUCAO-1)
+				fwrite(&aux[0], sizeof(char), 1, F);
+
+		};
+		fwrite(&aux[1], sizeof(char), 1, F);
+	}
+	fclose(F);
+}
+
+void binariza(unsigned char src[RESOLUCAO][RESOLUCAO][21], unsigned char image[RESOLUCAO][RESOLUCAO][21], int m) {
+	int i, j;
+
+	printf("\nImagem %d:\n", m+1);
+	for(i=0; i<RESOLUCAO; i++) {
+		for(j=0; j<RESOLUCAO; j++) {
+			if(src[i][j][m] < 20)
+				image[i][j][m] = 0;
+			else image[i][j][m] = 100;
+		}
+	}
+}
+
+int main(void) {
+	unsigned char src[RESOLUCAO][RESOLUCAO][21];
+	unsigned char baixa[RESOLUCAO][RESOLUCAO][21];
+	unsigned char alta[RESOLUCAO][RESOLUCAO][21];
+	int i;
+	char fileName[21] = "ImageIrDA/dataxx.csv";
+	char fileNameS[22] = "ImageIrDA/ALTASxx.csv";
+	char fileNameT[22] = "ImageIrDA/BAIXAxx.csv";
+
+	clock_t tempoInicial, tempoFinal;
+	double tempoGasto;
+	tempoInicial = clock();
+
+	for(i=1; i<=20; i++) {
+		fileName[14] = (i/10) + 0x30;		// Itera nos nomes dos arquivos
+		fileName[15] = (i%10) + 0x30;
+
+		imRead(fileName, src, (i-1));
+	}
+	//showImage(src);
+
+	for(i=0; i<20; i++) {
+
+		mediana(src, baixa, i, 3);
+
+	}
+	//showImage(baixa);
+
+	for(i=0; i<20; i++) {
+
+		laplaciano(baixa, alta, i);
+
+	}
+	//showImage(src);
+
+	for(i=1; i<=20; i++) {
+		fileNameT[15] = (i/10) + 0x30;		// Itera nos nomes dos arquivos
+		fileNameT[16] = (i%10) + 0x30;
+
+		imWrite(fileNameT, baixa, (i-1));
+	}
+
+	for(i=1; i<=20; i++) {
+		fileNameS[15] = (i/10) + 0x30;		// Itera nos nomes dos arquivos
+		fileNameS[16] = (i%10) + 0x30;
+
+		imWrite(fileNameS, alta, (i-1));
+	}
+
+	tempoFinal = clock();
+
+	tempoGasto = ((double)(tempoFinal-tempoInicial)/CLOCKS_PER_SEC)*100;
+	printf("\nTempo: %lf ms\n", tempoGasto);
 
 	return 0;
 }
